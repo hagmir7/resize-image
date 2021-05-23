92e20ceb83321ea36e109e0743b66008ffc2ce85
Hassan Agmir <hagmir6@gmail.com>
2021-05-23 20:10:52 +0100

    add resize countitiy

--- js/script.js
+++ js/script.js
@@ -10,19 +10,27 @@ function resize(){
         imageElement.onload = function(e){
             const canvas = document.createElement('canvas');
             if(e.target.width > 500 || e.target.height > 500){
-                const  max_width = 400;
+                const importWidth = document.getElementById('width').value;
+                const  max_width = parseInt(importWidth);
                 const scaleSize = max_width / e.target.width;
                 canvas.width = max_width;
                 canvas.height = e.target.height * scaleSize;
                 const ctx = canvas.getContext("2d");
                 ctx.drawImage(e.target, 0, 0, canvas.width, canvas.height);
                 const srcinclded = ctx.canvas.toDataURL(e.target, "iamge/jpeg", );
-                document.querySelector('#new').src =  srcinclded;
+                if (importWidth.length == 0){
+                    alert('You need to specify the width first!')
+                    document.querySelector('#olde').src = '';
+                }else{
+                    document.querySelector('#new').src =  srcinclded;
+                    const download = document.getElementById('download');
+                    download.classList.remove('hidden');
+                    const oldImage = document.querySelector('#olde').parentElement
+                    oldImage.classList.remove('hidden');
+                    oldImage.classList.add('d-flex');
+                    download.href = srcinclded
+                }
                 
-                const download = document.getElementById('download');
-                download.classList.remove('hidden');
-                const oldImage = document.querySelector('#olde').parentElement
-                oldImage.classList.remove('hidden');
-                oldImage.classList.add('d-flex');
-                download.href = srcinclded
+                
+
             };};};};
